60693a678b7a85a20c815725e1b6abfa09e289e0
xunix huang <qiwei_huang@qq.com>
2021-10-12 09:46:46 +0800

    reformat 6

--- 6/main.go
+++ 6/main.go
@@ -1,19 +1,21 @@
 package _6
 
 type ListNode struct {
-    Val  int
-    Next *ListNode
+	Val  int
+	Next *ListNode
 }
 
 func reversePrint(head *ListNode) []int {
-    var result []int
-    current := head
-    for current != nil {
-        result = append(result, current.Val)
-        current = current.Next
-    }
-    for i := 0; i < len(result)>>1; i++ {
-        result[i], result[len(result)-1-i] = result[len(result)-1-i], result[i]
-    }
-    return result
+	var result []int
+	var current = head
+	for current != nil {
+		result = append(result, current.Val)
+		current = current.Next
+	}
+
+	for i := 0; i < len(result)>>1; i++ {
+		result[i], result[len(result)-1-i] = result[len(result)-1-i], result[i]
+	}
+
+	return result
 }
